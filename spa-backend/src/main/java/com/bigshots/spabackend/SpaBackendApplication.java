8c58335268b34bb1767e0c58b3b8e9b57286bdd9
Andrew Krause <a-krause@onu.edu>
2022-05-28 20:47:45 -0400

    Adding Spring Boot backend

---
+++ spa-backend/src/main/java/com/bigshots/spabackend/SpaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bigshots.spabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaBackendApplication.class, args);
+	}
+
+}
