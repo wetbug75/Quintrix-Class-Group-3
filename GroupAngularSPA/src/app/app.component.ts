98a876be8d6edb36993971051254fed39e0a741d
Leon Lu <ltlu1993@gmail.com>
2022-05-29 22:26:31 -0400

    Added onclick functionality

--- GroupAngularSPA/src/app/app.component.ts
+++ GroupAngularSPA/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GroupAngularSPA';
+  
+  	jokeQuestion = '';
+  	//GET request
+	jokeAnswer = '';
+
+	getRandomJoke(){
+		@ViewChild('jokeQuestion') jQuestion: ElementRef;
+		this.jQuestion.nativeElement.innerHTML = '';
+		
+		@ViewChild('jokeAnswer') jAnswer: ElementRef;
+		this.jAnswer.nativeElement.innerHTML = '';
+	}
 }
