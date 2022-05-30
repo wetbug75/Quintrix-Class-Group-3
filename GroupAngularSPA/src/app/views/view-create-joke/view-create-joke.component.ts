f2d8093228b81ba46c025a09402ae5ac21929df8
simonsimon9 <simonkuang9@outlook.com>
2022-05-29 22:01:12 -0400

    created views folder with create joke view component, and added create joke selector to app.component.html

---
+++ GroupAngularSPA/src/app/views/view-create-joke/view-create-joke.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-create-joke',
+  templateUrl: './view-create-joke.component.html',
+  styleUrls: ['./view-create-joke.component.css']
+})
+export class ViewCreateJokeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
