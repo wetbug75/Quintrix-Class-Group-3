91e8185969393c83131e328fed224da47f26149e
Leon Lu <ltlu1993@gmail.com>
2022-05-31 10:26:46 -0400

    Created button components

--- GroupAngularSPA/src/app/app.component.ts
+++ GroupAngularSPA/src/app/app.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+  })
 export class AppComponent {
-  title = 'GroupAngularSPA';
+  title = 'The Big Shot Joke';
   
-  	jokeQuestion = '';
-  	//GET request
-	jokeAnswer = '';
-
 	getRandomJoke(){
-		@ViewChild('jokeQuestion') jQuestion: ElementRef;
-		this.jQuestion.nativeElement.innerHTML = '';
 		
-		@ViewChild('jokeAnswer') jAnswer: ElementRef;
-		this.jAnswer.nativeElement.innerHTML = '';
 	}
 }
--- GroupAngularSPA/src/app/app.module.ts
+++ GroupAngularSPA/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
