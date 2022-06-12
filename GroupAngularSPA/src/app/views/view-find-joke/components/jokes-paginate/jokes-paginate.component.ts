a0164af756318736d73f50b7408a69e854183e05
simonsimon9 <simonkuang9@outlook.com>
2022-06-12 13:03:14 -0400

    added comments to pagination component for future reference

--- GroupAngularSPA/src/app/views/view-find-joke/components/jokes-paginate/jokes-paginate.component.ts
+++ GroupAngularSPA/src/app/views/view-find-joke/components/jokes-paginate/jokes-paginate.component.ts
@@ -10,15 +10,15 @@ import { pageJoke } from 'src/app/models/pageJoke';
 })
 export class JokesPaginateComponent implements OnInit {
    JOKES: pageJoke[] = []; //recieved array of jokes from backend. 
-   totalElements: number = 0; //holds total no of records available in the database, server side paging useful. 
    currentPage: number = 1; // the current (active) page 
    pageSize: number= 6; //sets how many results per page
-   totalJokesDB: number;//need this from backend. 
-   keyword: string; //user keyword
+   totalJokesDB: number = 0;//need this from backend. This only updates the pagination controls. 
+   keyword: string = ""; //user keyword
    constructor(public jokeGetService: JokeGetService){
-      this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(jokes=>{
-        this.totalJokesDB = 150; //need this from backend. 
-        this.JOKES = jokes.filter(index => index !== null); //need joke array from backend. 
+        this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(jokes=>{
+          this.totalJokesDB = 150; //need this from backend. 
+          this.JOKES = jokes.filter(index => index !== null); //need joke array from backend. 
+          this.keyword = "";
       })
    }
 
@@ -26,25 +26,29 @@ export class JokesPaginateComponent implements OnInit {
    }
    //occurs when click on page number
    pageChanged(updatedPageNumber){ 
+     console.log(this.keyword); //keyword will be required to differentiate between getting with keyword or getting all jokes. 
      this.currentPage = updatedPageNumber;
      //find and retrieve all jokes. 
-     this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(result=>{
+     this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(result=>{ 
         console.log(result);
-        this.totalJokesDB =150;
+        this.totalJokesDB =150; //required from backend. 
         this.JOKES =result.filter(joke=> joke !== null);
       })
    }
 
+   //search keyword
    onSubmitSearch($event){
-     this.keyword = $event;
+    this.keyword = $event; //required - this is from the search bar. 
+    console.log(this.keyword) 
+    this.currentPage = 1; //required - sets to first page of results when user clicks on search
      
-     //mock - remove when done. 
-      this.pageSize = 3;
+    this.pageSize = 4; //remove - i think i can remove this function after the backend search keyword is enabled. 
+
      //search by key word
-     this.jokeGetService.getJokesPage(5,this.pageSize).subscribe(result=>{
+    this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(result=>{
         console.log(result);
-        this.totalJokesDB = 1; // how many jokes with that keyword
-        this.JOKES = result; //found jokes with the key word
+        this.totalJokesDB = 3; //required - how many jokes with that keyword - controls pagination control
+        this.JOKES = result.filter(joke=> joke!=null); //found jokes with the key word
       })
    }
 
