6e032e6df614a945eb5c200af8ab8b6ac9af1364
wetbug75 <a-krause@onu.edu>
2022-06-12 20:35:22 -0400

    changed User slightly and added OAuth dependancies

---
+++ spa-backend/src/main/java/com/bigshots/spabackend/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.bigshots.spabackend.config;
+ 
+import javax.sql.DataSource;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ 
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+    @Autowired
+    private DataSource dataSource;
+     
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username, password, enabled from users where username=?")
+            .authoritiesByUsernameQuery("select username, role from users where username=?")
+        ;
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll()
+            .and()
+            .logout().permitAll();     
+    }
+}
