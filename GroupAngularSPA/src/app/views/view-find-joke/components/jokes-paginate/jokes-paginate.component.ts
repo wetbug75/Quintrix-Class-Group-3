c56db0eaa57ef45f76ba624281eb7bf73deba03b
simonsimon9 <simonkuang9@outlook.com>
2022-06-15 13:38:52 -0400

    add count to front end for pagination

--- GroupAngularSPA/src/app/views/view-find-joke/components/jokes-paginate/jokes-paginate.component.ts
+++ GroupAngularSPA/src/app/views/view-find-joke/components/jokes-paginate/jokes-paginate.component.ts
@@ -13,43 +13,49 @@ export class JokesPaginateComponent implements OnInit {
    currentPage: number = 1; // the current (active) page 
    pageSize: number= 6; //sets how many results per page
    totalJokesDB: number = 0;//need this from backend. This only updates the pagination controls. 
-   keyword: string = ""; //user keyword
+   keyword: string; //user keyword
    constructor(public jokeGetService: JokeGetService){
-        this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(jokes=>{
-          this.totalJokesDB = 150; //need this from backend. 
-          this.JOKES = jokes.filter(index => index !== null); //need joke array from backend. 
-          this.keyword = "";
+      this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(jokes=>{ 
+        this.JOKES = jokes.filter(index => index !== null); //need joke array from backend. 
+        this.keyword = "";
       })
+      this.jokeGetService.getJokeSize().subscribe(count=>{
+        this.totalJokesDB = count;
+      })
+
    }
 
    ngOnInit(): void {
    }
    //occurs when click on page number
    pageChanged(updatedPageNumber){ 
+    console.log("this is keyword");
      console.log(this.keyword); //keyword will be required to differentiate between getting with keyword or getting all jokes. 
      this.currentPage = updatedPageNumber;
      //find and retrieve all jokes. 
      this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(result=>{ 
         console.log(result);
-        this.totalJokesDB =150; //required from backend. 
         this.JOKES =result.filter(joke=> joke !== null);
+      })
+      this.jokeGetService.getJokeSize().subscribe(count=>{
+        this.totalJokesDB = count;
       })
    }
 
-   //search keyword
+   //search keyword still in rogress
    onSubmitSearch($event){
     this.keyword = $event; //required - this is from the search bar. 
     console.log(this.keyword) 
     this.currentPage = 1; //required - sets to first page of results when user clicks on search
-     
-    this.pageSize = 4; //remove - i think i can remove this function after the backend search keyword is enabled. 
+    this.totalJokesDB = 3;
+    this.pageSize = 1; //remove - i think i can remove this function after the backend search keyword is enabled. 
 
      //search by key word
     this.jokeGetService.getJokesPage(this.currentPage,this.pageSize).subscribe(result=>{
         console.log(result);
-        this.totalJokesDB = 3; //required - how many jokes with that keyword - controls pagination control
         this.JOKES = result.filter(joke=> joke!=null); //found jokes with the key word
       })
+   
    }
 
    
