d812215f291d4465068af49f8333089831e656b6
simonsimon9 <simonkuang9@outlook.com>
2022-06-17 08:26:19 -0400

    first commit removed NoArgsConstructor

--- spa-backend/src/main/java/com/bigshots/spabackend/model/Joke.java
+++ spa-backend/src/main/java/com/bigshots/spabackend/model/Joke.java
@@ -14,7 +14,6 @@ import lombok.NoArgsConstructor;
 //import javax.persistence.Id;
 import javax.persistence.*;
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Container(containerName = "jokeKeywords")
 public class Joke {
--- spa-backend/src/main/java/com/bigshots/spabackend/model/Users.java
+++ spa-backend/src/main/java/com/bigshots/spabackend/model/Users.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
