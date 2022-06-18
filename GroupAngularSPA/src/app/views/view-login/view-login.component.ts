f1c6a88e62205c0a0b2b543a9b311559f660aba2
simonsimon9 <simonkuang9@outlook.com>
2022-06-18 08:45:32 -0400

    login and register message disappears after 3 seconds, login view is too big , need to refactor later

--- GroupAngularSPA/src/app/views/view-login/view-login.component.ts
+++ GroupAngularSPA/src/app/views/view-login/view-login.component.ts
@@ -4,23 +4,31 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserPostService } from 'src/app/core/services/UserPOST/user-post.service';
 import { Users } from 'src/app/models/User';
 import { AuthenticationService } from 'src/app/core/services/Authentication/authentication.service';
+import { Status } from 'src/app/models/status';
 @Component({
   selector: 'app-view-login',
   templateUrl: './view-login.component.html',
   styleUrls: ['./view-login.component.css']
 })
 export class ViewLoginComponent implements OnInit {
+  //Class too complicated. Needs refactoring. Too many
+  //states
   onCreateAccountForm:boolean;
 
-  errorMessage = 'Invalid Credentials';
+  errorMessage : string= 'Invalid Credentials';
+  registerSuccessMessage: string = 'Account created. Please login';
+  registerFailMessage: string = 'Something went wrong, try again or try later';
   
-  invalidLogin = false;
-  
+  invalidLoginAlert : boolean= false;
+  successRegisterAlert : boolean = false;
+  notsuccessRegisterAlert: boolean = false;
+
 
   constructor(private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService, public userPost: UserPostService) { 
     this.onCreateAccountForm = false;
+
   }
 
   ngOnInit(): void {
@@ -32,10 +40,11 @@ export class ViewLoginComponent implements OnInit {
     //this is where we will use service to send the user and password
     //TODO
     this.authenticationService.authenticationService(userLogin).subscribe((result)=> {
-      this.invalidLogin = false;
+      this.invalidLoginAlert = false;
       this.router.navigate(['']); //navigate to the 
     }, (error) => {
-      this.invalidLogin = true;
+      this.invalidLoginAlert = true;
+      setTimeout(() => {this.invalidLoginAlert=false},3000);
     });  
 
   }
@@ -47,12 +56,15 @@ export class ViewLoginComponent implements OnInit {
 
   //create new user 
   onRegisterFormGroupSubmit(userRegisterInfo: Users){
-    console.log(userRegisterInfo)
-     console.log("INSIDE NEW USER");
     //this is where we will use service to send to backend
     //for registration
     this.userPost.registerUser(userRegisterInfo).subscribe(result=>{
-      console.log(result);
+      this.onCreateAccountForm = false;
+      this.successRegisterAlert = true;
+      setTimeout(() => {this.successRegisterAlert=false},3000);
+    },(error)=>{
+      this.notsuccessRegisterAlert = true;
+      setTimeout(() => {this.notsuccessRegisterAlert=false},3000);
     })
   }
   
