b16842b33f85630c3bba12199368f99b9081739b
simonsimon9 <simonkuang9@outlook.com>
2022-07-05 10:52:05 -0400

    added unit test to Register Button

--- GroupAngularSPA/src/app/views/view-login/components/register-btn/register-btn.component.spec.ts
+++ GroupAngularSPA/src/app/views/view-login/components/register-btn/register-btn.component.spec.ts
@@ -1,8 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { RegisterBtnComponent } from './register-btn.component';
 
 describe('RegisterBtnComponent', () => {
+  //https://braydoncoyer.dev/blog/learn-how-to-click-a-button-when-angular-unit-testing
+
   let component: RegisterBtnComponent;
   let fixture: ComponentFixture<RegisterBtnComponent>;
 
@@ -21,5 +23,27 @@ describe('RegisterBtnComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call #onRegisterSubmit when clicked', fakeAsync(()=>{
+    spyOn(component, 'onRegisterSubmit');
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    expect(component.onRegisterSubmit).toHaveBeenCalled();
+  }));
+ 
+  it('should emit the event when #onRegisterSubmit is called',()=>{
+    const emitSpy = spyOn(component.onUserWantsNewAccount, 'emit');
+
+    component.onRegisterSubmit();
+
+    expect(emitSpy).toHaveBeenCalled();
+  })
+
+  it('should emit true when #onRegisterSubmit is cicked',()=>{
+    spyOn(component.onUserWantsNewAccount, 'emit');
+    component.onRegisterSubmit();
+    expect(component.onUserWantsNewAccount.emit).toHaveBeenCalledWith(true);
   });
 });
